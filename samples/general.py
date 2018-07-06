d813ff2a0ff387f9689429110cc5d2a5895fc0d1
rupeshmishra87 <rupeshmishra87@gmail.com>
2018-07-06 19:35:25 +0530

    Print odd/even minutes and some general functions

---
+++ samples/general.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+print("Current Directory : " + os.getcwd())
+print("Platform : " + sys.platform)
+print("Version : " + sys.version)
+print("Version Info : " + str(sys.version_info))
