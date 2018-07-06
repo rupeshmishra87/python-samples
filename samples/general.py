5b97488703aef72a09a7e0b7eb926d3b6ca7eca9
rupeshmishra87 <rupeshmishra87@gmail.com>
2018-07-06 20:04:13 +0530

    some general functions of python from standard library

--- samples/general.py
+++ samples/general.py
@@ -1,7 +1,21 @@
 import sys
 import os
+import datetime
+import time
 
+"""os module has functions to determine the information about the current runtime environment"""
+print("#### Environment specific information #####")
 print("Current Directory : " + os.getcwd())
+print("Environment Variables : " + str(os.environ))
+print("Current Home : " + os.getenv('HOME'))
+
+"""sys module has functions to determine information related to the system on which the interpreter is running"""
+print("#### Platform/System specific information #####")
 print("Platform : " + sys.platform)
 print("Version : " + sys.version)
 print("Version Info : " + str(sys.version_info))
+
+"""Date and Time related functions"""
+print("Un-formatted Date : " + str(datetime.date.today()))
+print("Formatted Date : " + str(datetime.date.isoformat(datetime.date.today())))
+print("Formatted Time : " + str(time.strftime("%H:%M,%A")))
